8f0913da751b425bf748001e43a3de09a6eeb5ad
Ayush <50929557+Ayushsrivastava15@users.noreply.github.com>
2021-09-07 00:56:43 +0530

    Add files via upload

---
+++ dwOpenSave.js
@@ -0,0 +1,38 @@
+let download=document.querySelector(".download");
+let open =document.querySelector(".open")
+let input=document.querySelector(".file_getter")
+let newFile=document.querySelector(".newFile");
+download.addEventListener("click",function(){
+    let a=document.createElement("a");
+    let url="data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(worksheetDB));
+    a.setAttribute("href",url );
+    a.setAttribute("download", "file.json");
+    a.click();
+    a.remove();
+})
+ open.addEventListener("click",function(){
+     input.click();
+     input.addEventListener("change",function(){
+         console.log("inside loop");
+        //  the files are saved in an array
+         let filesArr=input.files;
+        //  accesing file object from the file array
+         let fileObj=filesArr[0];
+        //  filereader is used for bigger files
+         let fr= new FileReader();
+         fr.readAsText(fileObj);
+         fr.addEventListener("load",function(){
+            let stringData=fr.result;
+            worksheetDB=JSON.parse(stringData);
+            sheetDB=worksheetDB[0];
+            setUI(sheetDB);  
+            let len=worksheetDB.length-1;
+            for (let i = 0; i < len; i++) {
+                addSheetBtn.click();
+            }
+         })
+     })
+ })
+ newFile.addEventListener("click",function(){
+     location.reload();
+ })
